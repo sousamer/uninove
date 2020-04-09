1cdce54e35f53f0d3391207169bbe238d47f239f
Wagner Souza <wagneersantos@gmail.com>
2020-04-09 10:57:25 -0300

    Create example 1

---
+++ programacao-web/aula-03/exemplo1.php
@@ -0,0 +1,23 @@
+<?php
+
+    $curso = array(
+        "UNINOVE", 
+        "PHP", 
+        "Programação Web", 
+        2020
+    );
+    
+    echo ( "Informações" .
+          "<br /><strong>Universidade</strong> ". $curso[0].
+          "<br />Linguagem ". $curso[1].
+          "<br />Curso ". $curso[2].
+          "<br />Ano ". $curso[3]
+        );
+    
+    
+    // echo "Universidade ". $curso[0];
+    // echo "Linguagem ".$curso[1];
+    // echo "Curso ".$curso[2];
+    // echo "Ano ".$curso[3];
+    
+?>
