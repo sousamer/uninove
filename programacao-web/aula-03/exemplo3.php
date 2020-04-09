9fe188350843ce6a6cc6e9be9531f2de45d3f943
Wagner Souza <wagneersantos@gmail.com>
2020-04-09 10:58:45 -0300

    Create example 3

---
+++ programacao-web/aula-03/exemplo3.php
@@ -0,0 +1,9 @@
+<?php
+    $aluno = array();
+    $aluno["nome"] = "Ronaldinho Gaúcho";
+    $aluno["idade"] = 37;
+    echo ("Olha o bruxo ai...".
+        "<br /> " . $aluno["nome"] .
+        "<br />Idade ". $aluno["idade"]
+    );
+?>
