9215a8dd0178f683474a5c452b3043c2f169daa0
Wagner Souza <wagneersantos@gmail.com>
2020-04-09 10:58:04 -0300

    Create example 2

---
+++ programacao-web/aula-03/exemplo2.php
@@ -0,0 +1,13 @@
+<?php
+
+    $carrinhoSuperMercado = array();
+    
+    $carrinhoSuperMercado[] = "Pão";
+    $carrinhoSuperMercado[] = "Refrigerante";
+    $carrinhoSuperMercado[] = "Sorvete";
+    
+    echo $carrinhoSuperMercado[0];
+    echo $carrinhoSuperMercado[1];
+    echo $carrinhoSuperMercado[2];
+
+?>
