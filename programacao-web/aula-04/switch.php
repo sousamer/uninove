36a01cb78f733725e96a2b592b5dd8acce704de0
Wagner Souza <wagneersantos@gmail.com>
2020-04-17 01:32:02 -0300

    Create file switch.php
    Creating a simple example

---
+++ programacao-web/aula-04/switch.php
@@ -0,0 +1,25 @@
+<?php
+    
+    $diaDaSemana = "Segunda";
+    
+    switch($diaDaSemana) {
+        case "Segunda": 
+            echo "O dia da semana é " . $diaDaSemana;
+            break;
+        case "Terça": 
+            echo "O dia da semana é " . $diaDaSemana;
+            break;
+        case "Quarta": 
+            echo "O dia da semana é " . $diaDaSemana;
+            break;
+        case "Quinta": 
+            echo "O dia da semana é " . $diaDaSemana;
+            break;
+        case "Sexta": 
+            echo "O dia da semana é " . $diaDaSemana;
+            break;
+        default:
+            echo "Dia não encontrado...";
+    }
+    
+?>
