4a15e60fb779cc1d6ee5d37c8783479cebcd3170
Wagner Souza <wagneersantos@gmail.com>
2020-04-17 01:29:03 -0300

    Create file if-elseif-else.php
    Create simple example

---
+++ programacao-web/aula-04/if-elseif-else.php
@@ -0,0 +1,13 @@
+<?php
+    
+    $nome = "João";
+    
+    if($nome == "João") {
+        echo "O nome é " . $nome;
+    } else if($name == "Maria") {
+        echo "O nome é " . $nome;
+    } else {
+        echo "Nome não encontrado..";
+    }
+    
+?>
